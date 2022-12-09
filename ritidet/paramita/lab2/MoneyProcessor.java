74eb9dc005008e1edbab442e29a6b96015767772
paramita-ri <paramita.ri@kkumail.com>
2022-12-09 11:35:52 +0700

    uddate code

---
+++ ritidet/paramita/lab2/MoneyProcessor.java
@@ -0,0 +1,13 @@
+package ritidet.paramita.lab2;
+
+public class MoneyProcessor {
+    public static void main(String[] args) {
+        int length_args = args.length ;
+        if (length_args == 4) {
+
+        }
+        else{
+            System.out.println("Usage");
+        }
+    }   
+}
