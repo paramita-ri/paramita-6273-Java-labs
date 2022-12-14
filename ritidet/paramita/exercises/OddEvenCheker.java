b5f063969c35a467e63e5ad3e63b1f9d2c8d05f5
paramita-ri <paramita.ri@kkumail.com>
2022-12-14 21:10:29 +0700

    OddEvenCheker

---
+++ ritidet/paramita/exercises/OddEvenCheker.java
@@ -0,0 +1,35 @@
+/**
+* This OddEvenCheker program is to accept one arguments: integer
+* and check that integer is even or odd number.
+* if integer is even
+* Its output format is 
+* “<integer> is even"
+* if integer is even
+* Its output format is 
+* “<integer> is odd"
+*
+* Author: Paramita Ritidet
+* ID: 653040627-3
+* Sec: 2
+* Date: December 14, 2022
+*
+**/
+
+package ritidet.paramita.exercises;
+
+public class OddEvenCheker {
+    public static void main(String[] args) {
+        int length_args = args.length;
+        if (length_args == 1) {
+            int integer = Integer.parseInt(args[0]);
+            if ((integer % 2) == 0) {
+                System.out.println(integer + " is even");
+            } else {
+                System.out.println(integer + " is odd");
+            }
+
+        } else {
+            System.out.println("Usage : OddEvenCheker <integer> ");
+        }
+    }
+}
