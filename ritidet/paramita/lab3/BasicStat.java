b2189bdfbf10fe49d5ab15060178c722ef4baaa0
paramita-ri <paramita.ri@kkumail.com>
2023-01-06 23:44:54 +0700

    uplosd basicstat

--- ritidet/paramita/lab3/BasicStat.java
+++ ritidet/paramita/lab3/BasicStat.java
@@ -1,15 +1,13 @@
 /**
-* This SicBo program is to accept two arguments to choose the game : 1 or 2
-* if players choose 1 players will play highlow game with 3 dice :
-*   choose the total of number that will be on 3 dice 
-*   if total of dice between 3-10 is low .if total of dice between 11-18 is high
-*   if players bet correctly he/she wins 20 baht .if players bet incorrectly he/she loose 10 baht
-* if players choose 2 players will play choosing number game with 3 dice :
-*   choose number between 1-6 that will be on 3 dice
-*   if players bets correct one dice he/she wins 30 baht
-*   if players bets correct two dice he/she wins 60 baht
-*   if players bets correct three dice he/she wins 90 baht
-*   if players bets incorrect he/she loose 10 baht
+* This BasicStat program is the program that will find the minimum, the maximum, the average,
+* the median, and the standard deviation of the list of numbers entered
+* Its output format is 
+* "Sorted numbers are <Sorted of numbers>
+* Minimum: <Minimum of numbers>
+* Maximum: <Maximum of numbers>
+* Average: <Average of numbers>
+* Median: <Median of numbers>
+* Standard deviation: <Standard deviation of numbers> "
 *
 * Author: Paramita Ritidet
 * ID: 653040627-3
@@ -19,10 +17,56 @@
 **/
 package ritidet.paramita.lab3;
 
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class BasicStat {
-    
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter numbers (separated by spaces): ");
+        String num_inputString = scanner.nextLine();
+        scanner.close();
+        String[] num_Strings = num_inputString.split(" ");
+
+        if (num_Strings.length >= 1 && num_Strings.length <= 10) {
+            double[] num_list = new double[num_Strings.length];
+            for (int i = 0; i < num_Strings.length; i++) {
+                num_list[i] = Double.parseDouble(num_Strings[i]);
+            }
+            Arrays.sort(num_list);
+            double min = num_list[0];
+            double max = num_list[num_list.length - 1];
+            double sum = 0;
+            for (double num : num_list) {
+                sum += num;
+            }
+            double avg = sum / num_list.length;
+            double median;
+            if (num_list.length % 2 == 0) {
+                median = (num_list[num_list.length / 2] + num_list[num_list.length / 2 - 1]) / 2;
+            } else {
+                median = num_list[num_list.length / 2];
+            }
+            double sumOfSquares = 0;
+            for (double num : num_list) {
+                sumOfSquares += Math.pow(num - avg, 2);
+            }
+            double standard_dev = Math.sqrt(sumOfSquares / num_list.length);
+            System.out.print("Sorted numbers are ");
+            for (double num : num_list) {
+            System.out.print(num + " ");
+            }
+            System.out.println("\nMinimum: " + min);
+            System.out.println("Maximum: " + max);
+            System.out.println("Average: " + avg);
+            System.out.println("Median: " + median);
+            DecimalFormat df = new DecimalFormat("#.##");
+            System.out.println("Standard deviation: " + df.format(standard_dev));
+        } else if (num_Strings.length == 0) {
+            System.out.println("please enter at least one number.");
+        } else {
+            System.out.println("Please enter no more than ten numbers.");
+        }
+    }
 }
