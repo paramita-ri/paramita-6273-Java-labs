c82d9759a805ee0337440cfcc19ca7c04724ad4a
paramita-ri <paramita.ri@kkumail.com>
2023-02-17 22:38:51 +0700

    commit AthleteFormV8

--- ritidet/paramita/exercises/AthleteFormV8.java
+++ ritidet/paramita/exercises/AthleteFormV8.java
@@ -1,9 +1,12 @@
 package ritidet.paramita.exercises;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import java.awt.event.*;
 
-public class AthleteFormV8 extends ritidet.paramita.lab7.AthleteForm {
-
+public class AthleteFormV8 extends ritidet.paramita.lab7.AthleteForm implements ActionListener {
+    protected String nameMessage, weightMessage, heightMessage, birthDateMessage, showMessage;
+    
     public AthleteFormV8(String title) {
         super(title);
     }
@@ -13,6 +16,26 @@ public class AthleteFormV8 extends ritidet.paramita.lab7.AthleteForm {
         msw.addComponents();
         msw.setFrameFeatures();
     }
-    
 
+    protected void addComponents() {
+        super.addComponents();
+        submitButton.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        nameMessage = nameField.getText();
+        weightMessage = weightField.getText();
+        heightMessage = heightField.getText();
+        birthDateMessage = birthDateField.getText();
+        showMessage = "Name: " + nameMessage + " Weight: " + weightMessage + " Height: " + heightMessage + " Birthday: " + birthDateMessage;
+        JOptionPane.showMessageDialog(this, showMessage, "Massege", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 }
